37ac7966a066f1476b93e9ad078b1ab9cf04536b
Jacopo De Stefani <jacopo.de.stefani@gmail.com>
2021-05-11 21:08:49 +0200

    First commit including ErrorMeasures.R and corresponding test. Tests and documentation still to be completed.

---
+++ R/ErrorMeasures.R
@@ -0,0 +1,325 @@
+################### Scale independant ########################
+
+#' MAPE - Mean Absolute Percentage Error
+#' 
+#' MAPE = \eqn{1/n \sum_{t=0}^n  100  (y_t - y_hat_t)/y_t}
+#' 
+#' @references Hyndman, R. J., & Koehler, A. B. (2006). Another look at measures of forecast accuracy. International journal of forecasting, 22(4), 679-688. 
+#' @param y - True values of the time series
+#' @param y_hat - Forecasted values of the time series
+#'
+#' @return MAPE value
+#' @examples
+MAPE <- function(y,y_hat,result_type=c("measure","raw")){
+  check_equal_length_pair(y,y_hat)
+  result_type <- match.arg(result_type)
+  return(switch(result_type,
+                measure = (1/length(y))*sum(abs(100*(y-y_hat)/y)),
+                raw = (1/length(y))*abs(100*(y-y_hat)/y),
+                MAPE(y,y_hat,"measure") # Default -> Recursive call with measure result type
+                )
+         )
+
+}
+
+#' MdAPE - Median Absolute Percentage Error
+#' \textbf{MdAPE} : $Md_{t \in \{1 \cdots n\}}(\mid 100 \cdot \frac{y_t - \hat{y}_t}{y_t}\mid)$
+#' Hyndman, R. J., & Koehler, A. B. (2006). Another look at measures of forecast accuracy. International journal of forecasting, 22(4), 679-688.
+#' @param y - True values of the time series
+#' @param y_hat - Forecasted values of the time series
+#'
+#' @return MdAPE value
+#' @examples
+MdAPE <- function(y,y_hat){
+  check_equal_length_pair(y,y_hat)
+  return(median(100*(y-y_hat)/y))
+}
+
+#' RMSPE - Root Mean Squared Percentage Error
+#' \textbf{RMSPE} : $\sqrt{\frac{1}{n} \sum_{t=0}^n (100 \cdot \frac{y_t - \hat{y}_t}{y_t})^2}$
+#' Hyndman, R. J., & Koehler, A. B. (2006). Another look at measures of forecast accuracy. International journal of forecasting, 22(4), 679-688.
+#' @param y - True values of the time series
+#' @param y_hat - Forecasted values of the time series
+#'
+#' @return RMSPE value
+#' @examples
+RMSPE <- function(y,y_hat,result_type=c("measure","raw")){
+  check_equal_length_pair(y,y_hat)
+  if(!missing(result_type) & length(result_type)>1){
+    stop("Only one 'result_type' allowed.")
+  }
+  result_type <- match.arg(result_type)
+  return(switch(result_type,
+                measure = sqrt((1/length(y))*sum(100*(((y-y_hat)/y)^2))),
+                raw = sqrt((1/length(y))*100*(((y-y_hat)/y)^2)),
+                RMSPE(y,y_hat,"measure") # Default -> Recursive call with measure result type
+                )
+        )
+
+}
+
+#' RMdSPE - Root Median Squared Percentage Error
+#' \textbf{RMdSPE} : $\sqrt{ Md_{t \in \{1 \cdots n\}}((100 \cdot \frac{y_t - \hat{y}_t}{y_t})^2)}$
+#' Hyndman, R. J., & Koehler, A. B. (2006). Another look at measures of forecast accuracy. International journal of forecasting, 22(4), 679-688.
+#' @param y - True values of the time series
+#' @param y_hat - Forecasted values of the time series
+#'
+#' @return RMdSPE value
+#' @examples
+RMdSPE <- function(y,y_hat){
+  check_equal_length_pair(y,y_hat)
+  return(sqrt(median(100*(((y-y_hat)/y)^2))))
+}
+
+#' sMAPE - Scaled Mean Absolute Percentage Error
+#' \textbf{sMAPE} : $\frac{100}{n} \sum_{t=0}^n \cdot \frac{\mid y_t - \hat{y}_t\mid}{\frac{y_t+\hat{y}_t}{2}}$
+#' Hyndman, R. J., & Koehler, A. B. (2006). Another look at measures of forecast accuracy. International journal of forecasting, 22(4), 679-688.
+#' @param y - True values of the time series
+#' @param y_hat - Forecasted values of the time series
+#'
+#' @return sMAPE value
+#' @examples
+sMAPE <- function(y,y_hat,result_type=c("measure","raw")){
+  check_equal_length_pair(y,y_hat)
+  if(!missing(result_type) & length(result_type)>1){
+    stop("Only one 'result_type' allowed.")
+  }
+  result_type <- match.arg(result_type)
+  return(switch(result_type,
+                measure = (200/length(y))*sum((abs(y-y_hat)/(y+y_hat))),
+                raw = (200/length(y))*(abs(y-y_hat)/(y+y_hat)),
+                sMAPE(y,y_hat,"measure") # Default -> Recursive call with measure result type
+                )
+    )
+}
+
+
+#' sMdAPE - Scaled Median Absolute Percentage Error
+#' \textbf{sMdAPE} : $Md_{t \in \{1 \cdots n\}}(200 \cdot \frac{\mid y_t - \hat{y}_t\mid}{y_t+\hat{y}_t})$
+#' Hyndman, R. J., & Koehler, A. B. (2006). Another look at measures of forecast accuracy. International journal of forecasting, 22(4), 679-688.
+#' @param y - True values of the time series
+#' @param y_hat - Forecasted values of the time series
+#'
+#' @return sMdAPE value
+#' @examples
+sMdAPE <- function(y,y_hat){
+  check_equal_length_pair(y,y_hat)
+  return(median(200*((abs(y-y_hat)/(y+y_hat)))))
+}
+
+################### Scale dependant ########################
+
+#' MSE - Mean Squared Error
+#' \textbf{MSE} : $ \frac{1}{n} \sum_{t=0}^n (y_t - \hat{y}_t)^2$
+#' Hyndman, R. J., & Koehler, A. B. (2006). Another look at measures of forecast accuracy. International journal of forecasting, 22(4), 679-688.
+#' @param y - True values of the time series
+#' @param y_hat - Forecasted values of the time series
+#'
+#' @return MSE value
+#' @examples
+MSE <- function(y,y_hat,result_type=c("measure","raw")){
+  check_equal_length_pair(y,y_hat)
+  if(!missing(result_type) & length(result_type)>1){
+    stop("Only one 'result_type' allowed.")
+  }
+  result_type <- match.arg(result_type)
+  return(switch(result_type,
+                measure = (1/length(y))*sum((y-y_hat)^2),
+                raw = (1/length(y))*(y-y_hat)^2,
+                MSE(y,y_hat,"measure") # Default -> Recursive call with measure result type
+                )
+  )
+}
+
+#' RMSE - Root Mean Squared Error
+#' \textbf{RMSE} : $\sqrt{ \frac{1}{n} \sum_{t=0}^n (y_t - \hat{y}_t)^2}$
+#' Hyndman, R. J., & Koehler, A. B. (2006). Another look at measures of forecast accuracy. International journal of forecasting, 22(4), 679-688.
+#' @param y - True values of the time series
+#' @param y_hat - Forecasted values of the time series
+#'
+#' @return MSE value
+#' @examples
+RMSE <- function(y,y_hat,result_type=c("measure","raw")){
+  result_type <- match.arg(result_type)
+  return(sqrt(MSE(y,y_hat,result_type)))
+}
+
+#' MAE - Mean Absolute Error
+#' \textbf{MAE} : $\frac{1}{n} \sum_{t=0}^n |y_t - \hat{y}_t|$
+#' Hyndman, R. J., & Koehler, A. B. (2006). Another look at measures of forecast accuracy. International journal of forecasting, 22(4), 679-688.
+#' @param y - True values of the time series
+#' @param y_hat - Forecasted values of the time series
+#'
+#' @return MAE value
+#' @examples
+MAE <- function(y,y_hat,result_type=c("measure","raw")){
+  check_equal_length_pair(y,y_hat)
+  if(!missing(result_type) & length(result_type)>1){
+    stop("Only one 'result_type' allowed.")
+  }
+  result_type <- match.arg(result_type)
+  return(switch(result_type,
+                measure = (1/length(y))*sum(abs(y-y_hat)),
+                raw = (1/length(y))*abs(y-y_hat),
+                MAE(y,y_hat,"measure") # Default -> Recursive call with measure result type
+               )
+  )
+}
+
+#' MdAE - Median Absolute Error
+#' \textbf{MdAE} : $Md_{t \in \{1 \cdots n\}}(|y_t - \hat{y}_t|)$
+#' Hyndman, R. J., & Koehler, A. B. (2006). Another look at measures of forecast accuracy. International journal of forecasting, 22(4), 679-688.
+#' @param y - True values of the time series
+#' @param y_hat - Forecasted values of the time series
+#'
+#' @return MdAE value
+#' @examples
+MdAE <- function(y,y_hat){
+  check_equal_length_pair(y,y_hat)
+  return(median(abs(y-y_hat)))
+}
+
+
+################### Relative Errors ########################
+
+#' RE - Relative Error
+#' \textbf{RE} : $\frac{\mathbf{y}-\hat{\mathbf{y}}}{\mathbf{y}-\hat{\mathbf{y}_b}}$
+#' @param y - True values of the time series
+#' @param y_hat - Forecasted values of the time series
+#' @param y_hat_bench - Forecasted values of the time series using the benchmark model
+#'
+#' @return RE value
+#' @examples
+RE <- function(y,y_hat,y_hat_bench){
+  check_equal_length_trio(y,y_hat,y_hat_bench)
+  return((y-y_hat) / (y-y_hat_bench))
+}
+
+#' MRAE - Mean Relative Absolute Error
+#' \textbf{MRAE} : $\frac{1}{n} \sum_{t=0}^n  \mid r_t \mid$
+#' @param y - True values of the time series
+#' @param y_hat - Forecasted values of the time series
+#' @param y_hat_bench - Forecasted values of the time series using the benchmark model
+#'
+#' @return MRAE value
+#' @examples
+MRAE <- function(y,y_hat,y_hat_bench,result_type=c("measure","raw")){
+  if(!missing(result_type) & length(result_type)>1){
+    stop("Only one 'result_type' allowed.")
+  }
+  result_type <- match.arg(result_type)
+  return(switch(result_type,
+                measure = (1/length(y))*sum(abs(RE(y,y_hat,y_hat_bench))),
+                raw = (1/length(y))*abs(RE(y,y_hat,y_hat_bench)),
+                MRAE(y,y_hat,"measure") # Default -> Recursive call with measure result type
+               )
+  )
+}
+
+
+#' MdRAE - Median Relative Absolute Error
+#' \textbf{MdRAE} : $Md_{t \in \{1 \cdots n\}}(\mid r_t \mid)$
+#' @param y - True values of the time series
+#' @param y_hat - Forecasted values of the time series
+#' @param y_hat_bench - Forecasted values of the time series using the benchmark model
+#'
+#' @return MdRAE value
+#' @examples
+MdRAE <- function(y,y_hat,y_hat_bench){
+  return(median(abs(RE(y,y_hat,y_hat_bench))))
+}
+
+
+#' GMRAE - Geometric Mean Relative Absolute Error
+#' \textbf{GMRAE} : $\sqrt[n]{\frac{1}{n} \prod{t=0}^n  \mid r_t \mid}$
+#' @param y - True values of the time series
+#' @param y_hat - Forecasted values of the time series
+#' @param y_hat_bench - Forecasted values of the time series using the benchmark model
+#'
+#' @return RE value
+#' @examples
+GMRAE <- function(y,y_hat,y_hat_bench,result_type=c("measure","raw")){
+  if(!missing(result_type) & length(result_type)>1){
+    stop("Only one 'result_type' allowed.")
+  }
+  result_type <- match.arg(result_type)
+  return(switch(result_type,
+                measure = ((1/length(y))*prod(abs(RE(y,y_hat,y_hat_bench))))^(1/length(y)),
+                raw = ((1/length(y))*abs(RE(y,y_hat,y_hat_bench)))^(1/length(y)),
+                MRAE(y,y_hat,"measure") # Default -> Recursive call with measure result type
+               )
+  )
+}
+
+################### Normalized Errors ########################
+
+#' MASE - Mean Absolute Scaled Error
+#' \textbf{MASE} : $\frac{1}{T}\sum_{t=1}^T \left( \frac{\left| e_t \right|}{\frac{1}{T-1}\sum_{i=2}^T \left| Y_i-Y_{i-1} \right|}\right)$
+#' @param y - True values of the time series
+#' @param y_hat - Forecasted values of the time series
+#'
+#' @return MASE value
+#' @examples
+MASE <- function(y,y_hat,result_type=c("measure","raw")){
+  check_equal_length_pair(y,y_hat)
+  if(!missing(result_type) & length(result_type)>1){
+    stop("Only one 'result_type' allowed.")
+  }
+  if( length(y) == 1 || length(y_hat) == 1 ){
+    stop("NMSE undefined for horizon 1")
+  }
+  result_type <- match.arg(result_type)
+  return(switch(result_type,
+         measure = ((length(y)-1)/length(y))*(1/sum(abs(diff(y))))*sum(abs(y-y_hat)),
+         raw = ((length(y)-1)/length(y))*(1/sum(abs(diff(y))))*abs(y-y_hat),
+         MASE(y,y_hat,"measure") # Default -> Recursive call with result_type measure
+         )
+  )
+}
+
+
+#' NMSE - Normalized Mean Squared Error
+#' \textbf{NMSE:} $\frac{1}{n}\frac{\sum_{t=0}^{n} (y_t-\hat{y}_t)^2}{var(y_t)} $
+#' @param y - True values of the time series
+#' @param y_hat - Forecasted values of the time series
+#' @param normalizing_variance - Variance of the training set
+#'
+#' @return NMSE value
+#' @examples
+NMSE <- function(y,y_hat,normalizing_variance=NULL,result_type=c("measure","raw")){
+  if(!missing(result_type) & length(result_type)>1){
+    stop("Only one 'result_type' allowed.")
+  }
+  if( length(y) == 1 || length(y_hat) == 1 ){
+    stop("NMSE undefined for horizon 1")
+  }
+  
+  if(is.null(normalizing_variance)){
+    normalizing_variance <- var(y)
+  }
+  
+  result_type <- match.arg(result_type)
+  return(MSE(y,y_hat,result_type)/normalizing_variance)
+}
+
+#' NNMSE - Normalized Naive Mean Squared Error
+#' \textbf{NMSE:} $\frac{1}{n}\frac{\sum_{t=0}^{n} (y_t-\hat{y}_t)^2}{\sum_{t=0}^{n} (y_t-\hat{y}_{Naive,t})^2} $
+#' @param y - True values of the time series
+#' @param y_hat - Forecasted values of the time series
+#' @param y_hat_naive - Forecasted values of the time series with the naive method
+#'
+#' @return NNMSE value
+#' @examples
+NNMSE <- function(y,y_hat,y_hat_naive,result_type=c("measure","raw")){
+  if(!missing(result_type) & length(result_type)>1){
+    stop("Only one 'result_type' allowed.")
+  }
+  
+  result_type <- match.arg(result_type)
+  return(switch(result_type,
+                measure = ,
+                raw = MSE(y,y_hat,result_type)/MSE(y,y_hat_naive,result_type),
+                NNMSE(y,y_hat,y_hat_naive,"measure") # Default -> Recursive call with result_type measure
+                )
+         )
+}
---
+++ tests/testthat/test-errorMeasures.R
@@ -0,0 +1,191 @@
+y <- AirPassengers
+splitting_point <- round(2*length(y)/3)
+y_train <- y[1:splitting_point]
+h <- 5
+
+y_test <- y[(splitting_point+1):(splitting_point+h)]
+y_hat_naive <- rep(tail(y_train,1),h)
+y_hat_average <-  rep(mean(y_train),h)
+
+expected_scalar_dimension <- 1
+
+################### Scale independant ########################
+
+test_that("MAPE - measure", {
+  result <- MAPE(y_test,y_hat_naive)
+  expect_true(is.numeric(result))
+  expect_equal(length(result),expected_scalar_dimension)
+})
+
+test_that("MAPE - raw", {
+  result <- MAE(y_test,y_hat_naive,result_type = "raw")
+  expect_true(is.numeric(result))
+  expect_equal(length(result),h)
+})
+
+test_that("RMSPE - measure", {
+  result <- RMSPE(y_test,y_hat_naive)
+  expect_true(is.numeric(result))
+  expect_equal(length(result),expected_scalar_dimension)
+})
+
+test_that("RMSPE - raw", {
+  result <- RMSPE(y_test,y_hat_naive,result_type = "raw")
+  expect_true(is.numeric(result))
+  expect_equal(length(result),h)
+})
+
+test_that("sMAPE - measure", {
+  result <- sMAPE(y_test,y_hat_naive)
+  expect_true(is.numeric(result))
+  expect_equal(length(result),expected_scalar_dimension)
+})
+
+test_that("sMAPE - raw", {
+  result <- sMAPE(y_test,y_hat_naive,result_type = "raw")
+  expect_true(is.numeric(result))
+  expect_equal(length(result),h)
+})
+
+test_that("MdAPE", {
+  result <- MdAPE(y_test,y_hat_naive)
+  expect_true(is.numeric(result))
+  expect_equal(length(result),expected_scalar_dimension)
+})
+
+test_that("RMdSPE", {
+  result <- RMdSPE(y_test,y_hat_naive)
+  expect_true(is.numeric(result))
+  expect_equal(length(result),expected_scalar_dimension)
+})
+
+test_that("sMdAPE", {
+  result <- sMdAPE(y_test,y_hat_naive)
+  expect_true(is.numeric(result))
+  expect_equal(length(result),expected_scalar_dimension)
+})
+
+################### Scale dependant ########################
+
+test_that("MSE - measure", {
+  result <- MSE(y_test,y_hat_naive)
+  expect_true(is.numeric(result))
+  expect_equal(length(result),expected_scalar_dimension)
+})
+
+test_that("MSE - raw", {
+  result <- MSE(y_test,y_hat_naive,result_type = "raw")
+  expect_true(is.numeric(result))
+  expect_equal(length(result),h)
+})
+
+test_that("RMSE - measure", {
+  result <- RMSE(y_test,y_hat_naive, result_type = "measure")
+  expect_true(is.numeric(result))
+  expect_equal(length(result),expected_scalar_dimension)
+})
+
+test_that("RMSE - raw", {
+  result <- RMSE(y_test,y_hat_naive,result_type = "raw")
+  expect_true(is.numeric(result))
+  expect_equal(length(result),h)
+})
+
+test_that("MAE - measure", {
+  result <- MAE(y_test,y_hat_naive)
+  expect_true(is.numeric(result))
+  expect_equal(length(result),expected_scalar_dimension)
+})
+
+test_that("MAE - raw", {
+  result <- MAE(y_test,y_hat_naive,result_type = "raw")
+  expect_true(is.numeric(result))
+  expect_equal(length(result),h)
+})
+
+test_that("MdAE", {
+  result <- MdAE(y_test,y_hat_naive)
+  expect_true(is.numeric(result))
+  expect_equal(length(result),expected_scalar_dimension)
+})
+
+
+################### Relative Errors ########################
+
+test_that("RE - measure", {
+  result <- RE(y_test,y_hat_average,y_hat_naive)
+  expect_true(is.numeric(result))
+  expect_equal(length(result),h)
+})
+
+test_that("MRAE - measure", {
+  result <- MRAE(y_test,y_hat_average,y_hat_naive)
+  expect_true(is.numeric(result))
+  expect_equal(length(result),expected_scalar_dimension)
+})
+
+test_that("MRAE - raw", {
+  result <- MRAE(y_test,y_hat_average,y_hat_naive,result_type = "raw")
+  expect_true(is.numeric(result))
+  expect_equal(length(result),h)
+})
+
+test_that("GMRAE - measure", {
+  result <- GMRAE(y_test,y_hat_average,y_hat_naive)
+  expect_true(is.numeric(result))
+  expect_equal(length(result),expected_scalar_dimension)
+})
+
+test_that("GMRAE - raw", {
+  result <- GMRAE(y_test,y_hat_average,y_hat_naive,result_type = "raw")
+  expect_true(is.numeric(result))
+  expect_equal(length(result),h)
+})
+
+test_that("MdRAE", {
+  result <- MdRAE(y_test,y_hat_average,y_hat_naive)
+  expect_true(is.numeric(result))
+  expect_equal(length(result),expected_scalar_dimension)
+})
+
+
+################### Normalized Errors ########################
+
+test_that("MASE - measure", {
+  result <- MASE(y_test,y_hat_naive)
+  expect_true(is.numeric(result))
+  expect_equal(length(result),expected_scalar_dimension)
+})
+
+test_that("MASE - raw", {
+  result <- MASE(y_test,y_hat_naive,result_type = "raw")
+  expect_true(is.numeric(result))
+  expect_equal(length(result),h)
+})
+
+test_that("NMSE - measure", {
+  result <- NMSE(y_test,y_hat_naive)
+  expect_true(is.numeric(result))
+  expect_equal(length(result),expected_scalar_dimension)
+})
+
+test_that("NMSE - raw", {
+  result <- NMSE(y_test,y_hat_naive,result_type = "raw")
+  expect_true(is.numeric(result))
+  expect_equal(length(result),h)
+})
+
+test_that("NNMSE - measure", {
+  result <- NNMSE(y_test,y_hat_average,y_hat_naive)
+  expect_true(is.numeric(result))
+  expect_equal(length(result),expected_scalar_dimension)
+})
+
+test_that("NNMSE - raw", {
+  result <- NNMSE(y_test,y_hat_average,y_hat_naive,result_type = "raw")
+  expect_true(is.numeric(result))
+  expect_equal(length(result),h)
+})
+
+
+
